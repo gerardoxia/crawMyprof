1d718a3b97e7ddfd5832ecb2c5a0a7052e74c36b
gerardoxia <gerardo.xia@outlook.com>
2016-03-26 14:29:37 -0500

    duke and umass

---
+++ tutorial/spiders/duke_spider.py
@@ -0,0 +1,30 @@
+import scrapy
+import re
+from tutorial.items import ProfessorItem
+
+
+class duke(scrapy.Spider):
+    name = "duke"
+    start_urls = [
+        "http://www.cs.duke.edu/people/faculty/",
+    ]
+
+    def parse(self, response):
+    	for index in range(2,47):
+    		str1=".//*[@id='Content']/table/tr[%d]" % index
+        	sel=response.xpath(str1)
+        	item = ProfessorItem()
+        	item['name']=sel.css(".PeopleName a::text").extract()[0]
+        	item['title']=sel.css(".ListTitle::text").extract()[0]
+        	item['email']=sel.css(".SubLine").re(r"Email(.*)</span>")[0]
+        	item['img']=sel.css(".Thumbnail::attr('src')").extract()[0]
+        	item['url']="http://www.cs.duke.edu/people/faculty/"+sel.css(".PeopleName a::attr('href')").extract()[0]
+        	url = item['url']
+        	request = scrapy.Request(url, callback=self.parse_prof_homepage)
+        	request.meta['item'] = item
+        	yield request
+        	yield item
+    def parse_prof_homepage(self, response):
+        item = response.meta['item']
+        if(response.xpath(".//*[@id='Content']").re(r"Research</p>\n(.*)")):item['area']=response.xpath(".//*[@id='Content']").re(r"Research</p>\n(.*)")[0]
+        return item
---
+++ tutorial/spiders/umass_spider.py
@@ -0,0 +1,28 @@
+import scrapy
+import re
+from tutorial.items import ProfessorItem
+
+
+class umass(scrapy.Spider):
+    name = "umass"
+    start_urls = [
+        "https://www.cics.umass.edu/faculty/faculty-directory",
+    ]
+
+    def parse(self, response):
+    	for sel in response.css(".view-content .views-row"):
+    		item = ProfessorItem()
+    		item['name']=sel.css(".field-name-title .even::text").extract()[0]
+    		item['title']=sel.css(".field-name-field-position .even::text").extract()[0]
+    		item['email']=sel.css(".field-name-field-email .even a::text").extract()[0]
+    		item['img']=sel.css(".field-name-field-image .even a>img::attr('src')").extract()[0]
+    		item['url']="https://www.cics.umass.edu"+sel.css(".field-name-field-image .even a::attr('href')").extract()[0]
+    		url = item['url']
+        	request = scrapy.Request(url, callback=self.parse_prof_homepage)
+        	request.meta['item'] = item
+        	yield request
+    		yield item
+    def parse_prof_homepage(self, response):
+        item = response.meta['item']
+        if(response.css(".content").re(r"Interests</div></h2><p>(.*?)</p>")):item['area']=response.css(".content").re(r"Interests</div></h2><p>(.*?)</p>")[0]
+        return item
