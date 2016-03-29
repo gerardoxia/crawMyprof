49c09347e4e82e2ad92ddce485d5b17311714454
gerardoxia <gerardo.xia@outlook.com>
2016-03-29 16:40:38 -0500

    cornell

--- tutorial/spiders/cornell_spider.py
+++ tutorial/spiders/cornell_spider.py
@@ -9,14 +9,14 @@ class cornell(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for sel in response.xpath(".//*[@id='node-93']/div/div/div/div/div/div"):
+        for sel in response.css(".person"):
             item = ProfessorItem()
-            item['name'] = sel.css("a > img::attr('alt')").extract()[0]
-            item['title'] = sel.xpath('./div/p[1]/text()').extract()[0].strip()
-            '''item['addr'] = selContent.xpath('./text()[2]').extract()[0].strip()
-            item['email'] = selContent.xpath('./a/text()').extract_first()
-            item['phone'] = selContent.xpath('./text()[4]').extract()[0].strip()
-            item['url'] = ", ".join(selContent.css("a:nth-of-type(3)::attr('href')").extract())'''
-            item['img'] = "https://www.cs.cornell.edu"+sel.css("a > img::attr('src')").extract()[0]
-            #item['area'] = ", ".join(sel.css('.views-field-field-research .item-list > ul > li > a::text').extract()).replace(u', and', u',')
+            item['name'] = sel.css(".info a::text").extract()[0]
+            item['url']=sel.css(".info a::attr('href')").extract()[0]
+            item['img']="http://www.cs.cornell.edu"+sel.css("img::attr('src')").extract()[0]
+            if(sel.css(".info").re(r"(.*Professor)")):item['title']=sel.css(".info").re(r"(.*Professor)")[0].strip().replace(u'<p>',u'')
+            elif(sel.css(".info").re(r"(.*Lecturer)")):item['title']=sel.css(".info").re(r"<p>(.*Lecturer)")[0]
+            else:item['title']=sel.css(".info").re(r"<p>(.*Scientist)")[0]
+            if(sel.css(".info a::attr('href')").re(r"edu/(.*)")):item['email']=sel.css(".info a::attr('href')").re(r"edu/(.*)")[0].replace(u'~',u'').replace(u'/',u'')+"@cs.cornell.edu"
+            item['area']=sel.css(".info").re(r"Res.*cus(.*)<")[0].strip().replace(u'</strong>',u'').replace(u'</b>','').replace(u':','')
             yield item
